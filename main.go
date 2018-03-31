5cc410f8e6f01b0372cc21dd6fc855550584ffcc
1348686364 <32665006+1348686364@users.noreply.github.com>
2018-03-31 11:11:55 +0800

    Update main.go

--- main.go
+++ main.go
@@ -43,7 +43,7 @@ func main() {
 func newMindCommand(cli *mindcli.MindCli) *cobra.Command {
 	var mindCmd = &cobra.Command{
 		Use:   "mind",
-		Short: "MIND Command-line Interface v0.6.1 For Multi Control(@ZXT)",
+		Short: "MIND Command-line Interface v0.6.1 For Multi Control(@Z)",
 	}
 	mindCmd.AddCommand(cmd.NewBuildCommand(cli))
 	mindCmd.AddCommand(cmd.NewPackCommand(cli))
